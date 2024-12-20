309e2ed4bbf6c13ca5c806602ee47d316caa9f63
gabsakura <gabsakura1@gmail.com>
2024-12-20 00:20:06 -0300

    ASD

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -57,6 +57,28 @@ const LoginForm = ({ onLogin }) => {
     setError('');
   };
 
+  const handleLogin = async (credentials) => {
+    try {
+      const response = await api.post('/api/login', {
+        email: credentials.email,
+        password: credentials.password
+      });
+
+      if (response.data.token) {
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', JSON.stringify(response.data.user));
+        api.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+        onLogin(response.data.user);
+      }
+    } catch (error) {
+      console.error('Erro no login:', {
+        message: error.response?.data?.message || error.message,
+        status: error.response?.status,
+        data: error.response?.data
+      });
+    }
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper
