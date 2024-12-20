b76e4678e8b03f66e9c2335f25dca4228dbf94c7
gabsakura <gabsakura1@gmail.com>
2024-12-20 00:26:57 -0300

    1231

--- start.js
+++ start.js
@@ -10,10 +10,36 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(compression())
-app.use(express.static('.'))
+app.use((req, res, next) => {
+  if (req.url.endsWith('.js')) {
+    res.type('application/javascript')
+  } else if (req.url.endsWith('.css')) {
+    res.type('text/css')
+  }
+  next()
+})
+
+app.use(express.static('.', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript')
+    } else if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css')
+    }
+  }
+}))
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'))
+  res.sendFile(join(__dirname, 'index.html'), {
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  })
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Algo deu errado!')
 })
 
 app.listen(PORT, '0.0.0.0', () => {
